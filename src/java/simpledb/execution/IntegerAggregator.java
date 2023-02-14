d02a0fa2f6632440368ed799bbd6a3dc1bf87f52
LibraTang <libratang@qq.com>
2023-02-14 18:09:46 +0800

    [Add] Implement IntegerAggregator

--- src/java/simpledb/execution/IntegerAggregator.java
+++ src/java/simpledb/execution/IntegerAggregator.java
@@ -1,7 +1,9 @@
 package simpledb.execution;
 
 import simpledb.common.Type;
-import simpledb.storage.Tuple;
+import simpledb.storage.*;
+
+import java.util.*;
 
 /**
  * Knows how to compute some aggregate over a set of IntFields.
@@ -10,23 +12,61 @@ public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
 
+    // group-by field index
+    private int gbfieldIndex;
+    // group-by field type
+    private Type gbfieldtype;
+    // aggregate field
+    private int afieldIndex;
+    // aggregation operator
+    private Op what;
+    // aggregator according to operator
+    private AbstractIntegerAggregator aggregator;
+
     /**
      * Aggregate constructor
      * 
-     * @param gbfield
+     * @param gbfieldIndex
      *            the 0-based index of the group-by field in the tuple, or
      *            NO_GROUPING if there is no grouping
      * @param gbfieldtype
      *            the type of the group by field (e.g., Type.INT_TYPE), or null
      *            if there is no grouping
-     * @param afield
+     * @param afieldIndex
      *            the 0-based index of the aggregate field in the tuple
      * @param what
      *            the aggregation operator
      */
 
-    public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
+    public IntegerAggregator(int gbfieldIndex, Type gbfieldtype, int afieldIndex, Op what) {
         // some code goes here
+        this.gbfieldIndex = gbfieldIndex;
+        this.gbfieldtype = gbfieldtype;
+        this.afieldIndex = afieldIndex;
+        this.what = what;
+        getAggregator();
+    }
+
+    private void getAggregator() {
+        switch (what) {
+            case COUNT:
+                this.aggregator = new IntegerCountAggregator();
+                break;
+            case SUM:
+                this.aggregator = new IntegerSumAggregator();
+                break;
+            case AVG:
+                this.aggregator = new IntegerAvgAggregator();
+                break;
+            case MIN:
+                this.aggregator = new IntegerMinAggregator();
+                break;
+            case MAX:
+                this.aggregator = new IntegerMaxAggregator();
+                break;
+            default:
+                throw new UnsupportedOperationException();
+        }
     }
 
     /**
@@ -38,6 +78,14 @@ public class IntegerAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+        Field gbField;
+        if (this.gbfieldIndex == NO_GROUPING) {
+            gbField = new IntField(0);
+        } else {
+            gbField = tup.getField(this.gbfieldIndex);
+        }
+        IntField aField = (IntField) tup.getField(afieldIndex);
+        this.aggregator.apply(gbField, aField.getValue());
     }
 
     /**
@@ -50,8 +98,110 @@ public class IntegerAggregator implements Aggregator {
      */
     public OpIterator iterator() {
         // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab2");
+        Map<Field, Integer> result = this.aggregator.result();
+        List<Tuple> tuples = new ArrayList<>();
+        TupleDesc td;
+        if (this.gbfieldIndex != NO_GROUPING) {
+            td = new TupleDesc(new Type[] {gbfieldtype, Type.INT_TYPE});
+            result.forEach((k, v) -> {
+                Tuple tuple = new Tuple(td);
+                tuple.setField(0, k);
+                tuple.setField(1, new IntField(v));
+                tuples.add(tuple);
+            });
+        } else {
+            td = new TupleDesc(new Type[]{Type.INT_TYPE});
+            result.forEach((k, v) -> {
+                Tuple tuple = new Tuple(td);
+                tuple.setField(0, new IntField(v));
+                tuples.add(tuple);
+            });
+        }
+        return new TupleIterator(td, tuples);
     }
 
+}
+
+abstract class AbstractIntegerAggregator {
+    // 按 group-by field 组织的map
+    protected Map<Field, Integer> groups = new HashMap<>();
+
+    public abstract void apply(Field group, Integer value);
+
+    public Map<Field, Integer> result() {
+        return groups;
+    }
+}
+
+class IntegerCountAggregator extends AbstractIntegerAggregator {
+    @Override
+    public void apply(Field group, Integer value) {
+        groups.compute(group, (k, v) -> {
+            if (v == null) {
+                return 1;
+            } else {
+                return v + 1;
+            }
+        });
+    }
+}
+
+class IntegerSumAggregator extends AbstractIntegerAggregator {
+    @Override
+    public void apply(Field group, Integer value) {
+        groups.compute(group, (k, v) -> {
+           if (v == null) {
+               return value;
+           } else {
+               return v + value;
+           }
+        });
+    }
+}
+
+class IntegerAvgAggregator extends AbstractIntegerAggregator {
+    private IntegerSumAggregator sumAggregator = new IntegerSumAggregator();
+    private IntegerCountAggregator countAggregator = new IntegerCountAggregator();
+
+    @Override
+    public void apply(Field group, Integer value) {
+        sumAggregator.apply(group, value);
+        countAggregator.apply(group, value);
+    }
+
+    @Override
+    public Map<Field, Integer> result() {
+        Map<Field, Integer> sumResult = sumAggregator.result();
+        Map<Field, Integer> countResult = countAggregator.result();
+
+        sumResult.forEach((group, sum) -> {
+            groups.put(group, sum / countResult.get(group));
+        });
+
+        return groups;
+    }
+}
+
+class IntegerMinAggregator extends AbstractIntegerAggregator {
+    @Override
+    public void apply(Field group, Integer value) {
+        groups.compute(group, (k, v) -> {
+            if (v == null) {
+                v = Integer.MAX_VALUE;
+            }
+            return Math.min(value, v);
+        });
+    }
+}
+
+class IntegerMaxAggregator extends AbstractIntegerAggregator {
+    @Override
+    public void apply(Field group, Integer value) {
+        groups.compute(group, (k, v) -> {
+            if (v == null) {
+                v = Integer.MIN_VALUE;
+            }
+            return Math.max(value, v);
+        });
+    }
 }
